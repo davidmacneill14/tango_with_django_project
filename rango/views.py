64413e91cbeb234b8a2307ec0424bce76feaca25
David MacNeill (student) <2381795M@student.gla.ac.uk>
2020-01-23 17:59:52 +0000

    Partially completed excercises for 3.6

--- rango/views.py
+++ rango/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hey there partner!")
+def about(request):
+    return HttpResponse("Rango says this is the about page.")
